664e86b476b1dcf89acaf7d61679477d1ea124c0
ritzier <ritzierwheat@gmail.com>
2024-07-26 00:59:41 +0800

    feat: Implemented mod Page * Implemented HomePage function (app/src/pages/home.rs) * Create a module for storing these Pages function (app/src/pages/mod.rs) * Modified lib code (app/src/lib.rs)

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,10 +1,12 @@
 use crate::error_template::{AppError, ErrorTemplate};
+use pages::*;
 
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
 pub mod error_template;
+mod pages;
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -29,18 +31,5 @@ pub fn App() -> impl IntoView {
                 </Routes>
             </main>
         </Router>
-    }
-}
-
-/// Renders the home page of your application.
-#[component]
-fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
-    view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
     }
 }
