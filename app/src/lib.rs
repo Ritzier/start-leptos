157db13fdcb5ab06e45987eb7c62747595bb46ec
ritzier <ritzierwheat@gmail.com>
2024-08-23 16:40:33 +0800

    Modified: App/lib.rs * Meta tag didn't close

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -14,21 +14,21 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-        <Stylesheet id="leptos" href="/pkg/leptos-workspace.css"/>
-        <Meta name="viewport" content="width=device-width, initial-scale=1">
+        <Stylesheet id="leptos" href="/pkg/leptos-workspace.css" />
+        <Meta name="viewport" content="width=device-width, initial-scale=1" />
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="Welcome to Leptos" />
 
         // content for this welcome page
         <Router fallback=|| {
             let mut outside_errors = Errors::default();
             outside_errors.insert_with_default_key(AppError::NotFound);
-            view! { <ErrorTemplate outside_errors/> }.into_view()
+            view! { <ErrorTemplate outside_errors /> }.into_view()
         }>
             <main>
                 <Routes>
-                    <Route path="" view=HomePage/>
+                    <Route path="" view=HomePage />
                 </Routes>
             </main>
         </Router>
