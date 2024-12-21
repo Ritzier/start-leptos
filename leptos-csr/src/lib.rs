c9050e464127dfef6ebd5569c7c30f651464097c
ritzier <ritzierwheat@gmail.com>
2024-12-21 22:17:20 +0800

    feat(template): add `leptos-csr` tempalte

---
+++ leptos-csr/src/lib.rs
@@ -0,0 +1,6 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! { <h2>"Hello, Leptos!"</h2> }
+}
---
+++ leptos-csr/src/main.rs
@@ -0,0 +1,10 @@
+use {{project-name}}::App;
+use leptos::prelude::*;
+
+pub fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    mount_to_body(|| {
+        view!{ <App/> }
+    })
+}
