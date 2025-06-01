f087faed5405c39620392da02cc658af38e78286
ritzier <ritzierwheat@gmail.com>
2025-06-02 05:07:13 +0800

    feat(project): remove `#[cfg]` in `src/main.rs`

--- leptos-project/src/main-normal.rs
+++ leptos-project/src/main-normal.rs
@@ -5,10 +5,3 @@ async fn main() {
 
     Server::setup().await;
 }
-
-#[cfg(not(feature = "ssr"))]
-pub fn main() {
-    // no client-side main function
-    // unless we want this to work with e.g., Trunk for pure client-side testing
-    // see lib.rs for hydration function instead
-}
--- leptos-project/src/main-tracing.rs
+++ leptos-project/src/main-tracing.rs
@@ -1,15 +1,7 @@
-#[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
     use {{project-name | snake_case}}::ssr::*;
 
     Trace::setup();
     Server::setup().await;
-}
-
-#[cfg(not(feature = "ssr"))]
-pub fn main() {
-    // no client-side main function
-    // unless we want this to work with e.g., Trunk for pure client-side testing
-    // see lib.rs for hydration function instead
 }
