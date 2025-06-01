e0d8d55e80e70458423d413939cacceae355ea77
ritzier <ritzierwheat@gmail.com>
2025-06-02 04:32:22 +0800

    feat(project): add `tracing-subscriber-wasm`

--- leptos-project/src/lib-tracing.rs
+++ leptos-project/src/lib-tracing.rs
@@ -7,6 +7,13 @@ pub mod ssr;
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
     use crate::app::*;
+    use tracing_subscriber::fmt;
+    use tracing_subscriber_wasm::MakeConsoleWriter;
+
+    fmt()
+        .with_writer(MakeConsoleWriter::default().map_trace_level_to(tracing::Level::DEBUG))
+        .without_time()
+        .init();
     console_error_panic_hook::set_once();
     leptos::mount::hydrate_body(App)
 }
