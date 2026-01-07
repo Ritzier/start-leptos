4c7f9a4168aee9de4692a71236920030861f783d
ritzier <ritzierwheat@gmail.com>
2026-01-07 21:53:28 +0800

    feat: modify affected code #2

--- leptos-workspace/app/src/app.rs
+++ leptos-workspace/app/src/app.rs
@@ -1,7 +1,7 @@
 use leptos::prelude::*;
 use leptos_meta::provide_meta_context;
 use leptos_router::components::{Route, Router, Routes};
-{% if lazy == "no" %}use leptos_router::path;{% else %}use leptos_router::{Lazy, path};{% endif %}
+use leptos_router::{Lazy, path};
 
 use crate::pages::*;
 
@@ -12,7 +12,7 @@ pub fn App() -> impl IntoView {
     view! {
         <Router>
             <Routes fallback=|| "Page not found".into_view()>
-                {% if lazy == "no" %}<Route path=path!("") view=HomePage />{% else %}<Route path=path!("") view={Lazy::<HomePage>::new()} />{% endif %}
+                <Route path=path!("") view={Lazy::<HomePage>::new()} />
             </Routes>
         </Router>
     }
--- leptos-workspace/frontend/src/lib.rs
+++ leptos-workspace/frontend/src/lib.rs
@@ -3,5 +3,5 @@ use app::*;
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
     console_error_panic_hook::set_once();
-    {% if lazy == "yes" %}leptos::mount::hydrate_lazy(App);{%else%}leptos::mount::hydrate_body(App);{% endif %}
+    leptos::mount::hydrate_lazy(App);
 }
